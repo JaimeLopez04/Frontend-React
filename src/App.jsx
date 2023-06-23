f36494906ef2263f3bf48a20ab056c7b6bced16b
tephaMR22 <stephania.martinez.rodrigurz@correounivalle.edu.co>
2023-06-22 21:57:54 -0500

    Reorganizacion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import Login from "./Login"
+import React from "react"
+import Login from "./auth/Login"
 
 function App() {
+  const [user, setUser] = React.useState();
+  const [auth, setAuthState] = React.useState();
 
   return (
     <>
