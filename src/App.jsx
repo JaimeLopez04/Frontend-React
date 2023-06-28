bae9dfb64dafc9c10aaad3d402dcafdd58fa9aac
tephaMR22 <stephania.martinez.rodrigurz@correounivalle.edu.co>
2023-06-28 10:08:24 -0500

    Cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import React from "react"
-import Sidebar from "./Components/side-bar"
-import SearchBar from "./Components/SearchBar"
-import Login from "./auth/Login"
 import Registro from "./auth/Registro";
-import CardSong from "./Components/CardSong";
-
 
 function App() {
 
     return (
         <>
-            {/* <Login/> */}
-            <div className="bg-black flex flex-row" >
-
-            {/* Barra Lateral */}
-                  <div className="w-full h-screen bg-back object-cover flex items-center">
-                  <Sidebar/>
-            </div>
-
-            <div className="left-[5rm]">
-                    <SearchBar/>
-            </div>
-
-
-
-             <Registro/>
+            <Login/>
         </>
     )
 }
