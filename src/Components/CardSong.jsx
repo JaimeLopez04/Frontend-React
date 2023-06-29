33fa6881f99f9d768b9413049eb6ff0690cf7204
tephaMR22 <stephania.martinez.rodrigurz@correounivalle.edu.co>
2023-06-28 21:30:01 -0500

    Creacion de la pagina Home

--- src/Components/CardSong.jsx
+++ src/Components/CardSong.jsx
@@ -4,9 +4,9 @@ const urlImage = "https://i.pinimg.com/564x/30/3d/83/303d83cb35d5e0ab0d8d6ac28c1
 
 const CardSong = () => {
     return (
-        <div className='bg-black/20  backdrop-blur-lg text-center rounded-xl items-center justify-center w-48 m-2'>
+        <div className='bg-white/10  backdrop-blur-lg text-center rounded-xl items-center justify-center w-48 m-2'>
             <div className='flex items-center justify-center'>
-                <img src={urlImage} alt="Imagen" className='rounded-md object-cover w-32 h-32' />
+                <img src={urlImage} alt="Imagen" className='rounded-md object-cover w-36 h-32 mt-2' />
             </div>
                 <div className='p-4 '>
                     <h2 className='font-semibold'>Nombre de la cancion</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,53 @@
 import Sidebar from "../Components/side-bar"
 import SearchBar from "../Components/SearchBar"
+import CardSong from "../Components/CardSong"
 
 function Home() {
 
     return (
         <>
-        <div className="flex flex-row">
-            <div className="w-full h-screen bg-back object-cover flex items-center">
-                <Sidebar/>
+            <div className="flex overflow-hidden">
+                <aside class="h-screen flex items-center justify-center fixed top-0 left-0 z-10">
+                    <Sidebar/>
+                </aside>
             </div>
-            <SearchBar/>
-            
-        </div>
+            <div className="flex flex-col w-full overflow-hidden relative">
+                <nav className="h-[4.5rem] flex justify-between fixed top-3 ml-[22rem] mr-2 inset-x-0 z-10 bg-white/10 items-center rounded-xl border border-solid border-white border-opacity-10 ">
+                    <h1 className="p-2 pl-6 pt-1 font-bold text-4xl text-white whitespace-pre opacity-100">Inicio</h1>
+                    <SearchBar/>
+                </nav>
+                <div class="flex">
+                    <main class="flex-auto min-h-screen p-4 overflow-hidden pt-24 pl-96 pr-60 mb-20">
+                        <div class=" grid grid-cols-2 gap-y-2 gap-x-56 lg:grid-cols-4 ">
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                            <CardSong/>
+                        </div>
+                    </main>
+                    <footer>
+                        <div className="bottom-3 inset-x-0 justify-center z-10 text-xl h-20 flex fixed bg-black/80 items-center ml-[22rem] mr-2">
+                            reproductor
+                        </div>
+                    </footer>
+                </div>
+            </div>
         </>
     )
 }
